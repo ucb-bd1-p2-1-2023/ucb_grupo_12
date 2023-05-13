7dec348f2ad51fad64882cd4009c4c204d03a044
nutella95oks <nathanaelalba@yahoo.com>
2023-05-12 23:34:13 -0800

    nuevo

--- server/main.js
+++ server/main.js
@@ -20,16 +20,18 @@ app.get('/', (req, res) => {
  res.send('API is working');
 })
 
+//connection.connect();
 
 app.post('/driver',(req, res) => {
+ //connection.connect();
  const body = req.body;
- const query = `INSERT INTO user(firstName, lastName, email) VALUES ('${body.firstName}', '${body.lastName}','${body.email}');`;
  connection.connect();
+ const query = `INSERT INTO user(firstName, lastName, email) VALUES ('${body.firstName}', '${body.lastName}','${body.email}');`;
  connection.query( query, (err, rows, fields) => {
    if (err) throw err
    console.log('1 record inserted');
  })
- connection.end();
+ //connection.end();
  res.send('1 record inserted');
 })
 
@@ -41,9 +43,10 @@ app.listen(port, () => {
 
 const connection = mysql.createConnection({
  host: 'localhost',
- port: '3306',
+ port: '3307',
  user: 'root',
  password: 'root',
  database: 'db1'
 })
 
+//connection.connect(()=> console.log("connection start"));
