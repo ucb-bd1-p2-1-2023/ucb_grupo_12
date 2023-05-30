d46068d1c2dfdba6833dbd72e93a8c0d12710b8d
nutella95oks <nathanaelalba@yahoo.com>
2023-05-29 17:56:14 -0800

    fianl

--- server/main.js
+++ server/main.js
@@ -14,19 +14,16 @@ app.use(cors({
 const port = 3000;
 
 
-
-
 app.get('/', (req, res) => {
  res.send('API is working');
 })
 
-//connection.connect();
-
+//craer para cada tabla
+//tabla user
 app.post('/driver',(req, res) => {
- //connection.connect();
  const body = req.body;
- connection.connect();
- const query = `INSERT INTO user(firstName, lastName, email) VALUES ('${body.firstName}', '${body.lastName}','${body.email}');`;
+ //connection.connect();
+ const query = `INSERT INTO user(firstName, lastName, email, telephone, password) VALUES ('${body.firstName}', '${body.lastName}','${body.email}', '${body.telephone}', '${body.password}');`;
  connection.query( query, (err, rows, fields) => {
    if (err) throw err
    console.log('1 record inserted');
@@ -34,6 +31,32 @@ app.post('/driver',(req, res) => {
  //connection.end();
  res.send('1 record inserted');
 })
+//tabla conductor
+app.post('/conductor',(req, res) => {
+  const body = req.body;
+  //connection.connect();
+  const query = `INSERT INTO conductor(idConductor,licensenumber,carmake,carmodel,carcolor,caryear,carlicenceplate) VALUES 
+  ('${body.idConductor}','${body.licensenumber}', '${body.carmake}','${body.carmodel}','${body.carcolor}','${body.caryear}','${body.carlicenceplate}');`;
+  connection.query( query, (err, rows, fields) => {
+    if (err) throw err
+    console.log('1 record inserted');
+  })
+  //connection.end();
+  res.send('1 record inserted');
+ })
+ 
+ //tabla registro_ubicacion
+ app.post('/registro_ubicacion',(req, res) => {
+  const body = req.body;
+  //connection.connect();
+  const query = `INSERT INTO registro_ubicacion(fecha, startLocation,endLocation, price) VALUES ('${body.fecha}', '${body. startLocation}','${body.endLocation}', '${body.price}');`;
+  connection.query( query, (err, rows, fields) => {
+    if (err) throw err
+    console.log('1 record inserted');
+  })
+  //connection.end();
+  res.send('1 record inserted');
+ })
 
 
 app.listen(port, () => {
@@ -43,10 +66,11 @@ app.listen(port, () => {
 
 const connection = mysql.createConnection({
  host: 'localhost',
- port: '3307',
+ port: '3306',
  user: 'root',
  password: 'root',
  database: 'db1'
 })
 
 //connection.connect(()=> console.log("connection start"));
+connection.connect();
